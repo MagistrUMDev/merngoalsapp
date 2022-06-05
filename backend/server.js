29fe5f490293283ec53dfb790f40a1dc02441bbf
MagistrUMDev <97807772+MagistrUMDev@users.noreply.github.com>
2022-06-05 23:19:08 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,16 +21,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(goalRouter);
 app.use(userRouter);
 
-// // Serve frontend
-// if(process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../frontend/build')))
+// Serve frontend
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-//   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
-// } else {
-//   app.get('/', (req, res) => {
-//     res.send('Please set to production')
-//   })
-// }
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+} else {
+  app.get('/', (req, res) => {
+    res.send('Please set to production')
+  })
+}
 
 app.use(errorHandler);
 
