469790099ef6d3212d34d86f4eaae11b274d135d
MagistrUMDev <97807772+MagistrUMDev@users.noreply.github.com>
2022-06-08 12:41:31 +0300

    Update authService.js

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://magistrumdevgoals.herokuapp.com/users/";
+const API_URL = "https://magistrumdevgoals.herokuapp.com/api/users/";
 
 // Register User
 const register = async (userData) => {
